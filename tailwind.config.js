7837cdb5a51c9d6494d523d0ae57dcf5615e4a32
unknown <cristiandavid.vj@gmail.com>
2025-03-04 15:17:04 -0500

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Para App Router
+    "./pages/**/*.{js,ts,jsx,tsx}", // Para Pages Router
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
